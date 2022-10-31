ded5e2f162904d452667a09775604296b64777b5
Sakugits <albermorcillodev@gmail.com>
2022-10-31 13:18:03 +0100

    Mas comentarios

--- ParteB/controladorB.c
+++ ParteB/controladorB.c
@@ -377,7 +377,7 @@ void *controller(void *arg)
 
         clock_gettime(CLOCK_MONOTONIC, &start);
 
-        switch (secondary_cycle_counter) //CP = 10, CS = 5
+        switch (secondary_cycle_counter) //CP = 10, CS = 5, cumple los requisitos
         {
         case 0:
             task_ligth_sensor();
